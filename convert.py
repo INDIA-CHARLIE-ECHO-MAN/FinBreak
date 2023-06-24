427d6fdf6d1ddca74a16b37a4fb2319c355b5827
INDIA-CHARLIE-ECHO-MAN <43439611+INDIA-CHARLIE-ECHO-MAN@users.noreply.github.com>
2023-06-24 21:53:38 +1000

    update gitignore to avoid .json and convert.py completed function to extract data, output in dict form. Need to Prep json conversion for database usage.

--- convert.py
+++ convert.py
@@ -1,16 +1,45 @@
 import re
-import os
+import json
+from datetime import datetime
 
 # Prototype to extract transactional data from Westpac transaction activity from web interface in csv file
 fileName = "test.txt"
-with open(fileName) as file:
-    # assign date, transaction details, value, final cost
+storage = "data.json"
+with open(storage, "w+") as store:
+    with open(fileName) as file:
+        # assign Transdate, transaction details, value, final cost, Date
 
-    for line in file:
-        convert = line.strip()
-        convert = convert.split('\t')
-        convert = [item for item in convert if item.strip()]
-        print(convert)
+        for line in file:
+            convert = line.strip()
+            convert = convert.split('\t')
+            convert = [item for item in convert if item.strip()]
 
+            transDate = convert[0]
+            transDate = str(datetime.strptime(transDate, '%d %b %Y'))[0:10]
+
+            detail = convert[1]
+            detail = re.sub(r'Click for details$', '', detail)
+            date = re.findall(r'[0-9][0-9]/[0-9][0-9]$', detail)
+            if (date == []):
+                date = None
+            else:
+                date = ''.join(date)
+            detail = (re.sub(r'[0-9][0-9]/[0-9][0-9]$', '', detail)).split()[3:]
+
+            isExpense = True
+            if (convert[2][0] == '$'):
+                isExpense = False
+
+            transDict = {
+                "transDate": transDate,
+                "date": date,
+                "detail": ' '.join(detail),
+                "transValue": convert[2],
+                "finAmount": convert[3],
+                "isExpense": isExpense
+            }
+
+            
+            print(transDict)
 
     
